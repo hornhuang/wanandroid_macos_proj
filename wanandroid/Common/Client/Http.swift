66d647c954085ffd9ab5c33c7d6a68c140747437
hornhuang <814484626@qq.com>
2021-09-14 16:40:55 +0800

    add: Catalogue init
    add: Catalogue init

---
+++ wanandroid/Common/Client/Http.swift
@@ -0,0 +1,8 @@
+//
+//  Http.swift
+//  wanandroid
+//
+//  Created by sunshine on 2021/9/14.
+//
+
+import Foundation
---
+++ wanandroid/Module/Model/LoginModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginModel.swift
+//  wanandroid
+//
+//  Created by sunshine on 2021/9/14.
+//
+
+import Foundation
