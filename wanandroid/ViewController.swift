ed47a6d2b6dcee0eb4bc817a4937360810224980
hornhuang <814484626@qq.com>
2021-09-14 16:25:02 +0800

    Initial Commit

---
+++ wanandroid/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  wanandroid
+//
+//  Created by sunshine on 2021/9/14.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
